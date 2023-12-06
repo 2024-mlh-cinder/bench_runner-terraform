bfd48a60fad08f51eec3c22c0454dac4237c4d8c
Johnston Jiaa <oclbdk@gmail.com>
2023-12-06 04:25:58 -0800

    fix repo for real

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "bench_runner" {
    curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz 2>&1 | tee curl-logs.txt
    tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz 2>&1 | tee tar-logs.txt
 
-   RUNNER_ALLOW_RUNASROOT="1" ./config.sh --url https://github.com/2014-mlh-cinder/bench_runner --token "${var.register_token}" --runnergroup "Default" --name "benchrunner" --labels "self-hosted,Linux,X64" --work "_work" 2>&1 | tee config-logs.txt
+   RUNNER_ALLOW_RUNASROOT="1" ./config.sh --url https://github.com/2014-mlh-cinder/bench_runner-results --token "${var.register_token}" --runnergroup "Default" --name "benchrunner" --labels "self-hosted,Linux,X64" --work "_work" 2>&1 | tee config-logs.txt
 
    ./run.sh
 
