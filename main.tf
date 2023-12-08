34d374ffbac2c4c72954fd7e58b46c58b9da4d2e
Johnston Jiaa <oclbdk@gmail.com>
2023-12-07 21:41:51 -0800

    fix sudo su USER, for real for real for real

--- main.tf
+++ main.tf
@@ -25,15 +25,15 @@ resource "aws_instance" "bench_runner" {
     sudo hostnamectl set-hostname cinder-benchmarking-aws
     sudo dnf install -y lttng-ust openssl-libs krb5-libs zlib libicu
 
-    su ec2-user <<'EOF'
+    su ec2-user <<'EOF2'
       cd ~ec2-user
       mkdir actions-runner && cd actions-runner
-      curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz 2>&1 | tee curl-logs.txt
-      tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz 2>&1 | tee tar-logs.txt
+      curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz
+      tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz
+      rm -f ./actions-runner-linux-x64-2.311.0.tar.gz
 
-      ./config.sh --url https://github.com/2014-mlh-cinder/bench_runner-results --token "${var.register_token}" --runnergroup "Default" --name "benchrunner" --labels "self-hosted,Linux,X64" --work "_work" 2>&1 | tee config-logs.txt
-      ./run.sh
-    EOF
+      ./config.sh --url https://github.com/2014-mlh-cinder/bench_runner-results --token "${var.register_token}" --runnergroup "Default" --name "benchrunner" --labels "self-hosted,Linux,X64" --work "_work"
+    EOF2
   EOF
 
   tags = {
